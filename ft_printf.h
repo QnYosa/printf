a99442c3e3f52af18df712f4bebecf25b4ca3e44
QnYosa <dimitriyoula@gmail.com>
2021-06-28 00:51:38 +0200

    + y padding me parecen hecho

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,21 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
+typedef struct width
+{
+	int zero; //if 0 else ' ' (seulement sur la gauche)
+	int steps; //(nombre de space) - (len du int) if int trop grand tout print;
+	int left; //boolean if '-'
+	char sign; // space or 0
+	int plus; //afficher un + devant les positifs
+	int visible; // invisible plus sign.
+
+}	width;
+
+
 void	ft_putstr(char *str);
 void	ft_putchar(char c);
+int     ft_atoi(const char *str);
+
 
 #endif
---
+++ width.c
@@ -0,0 +1,55 @@
+#include "ft_printf.h"
+
+int     ft_atoi(const char *str)
+{
+        int                     i;
+        int                     sign;
+        long long       number;
+
+        sign = 1;
+        i = 0;
+        number = 0;
+        while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+                i++;
+        if (str[i] == '-' || str[i] == '+')
+        {
+                if (str[i] == '-')
+                        sign *= -1;
+                i++;
+        }
+        while (str[i] >= '0' && str[i] <= '9')
+        {
+                number = number * 10 + (str[i] - '0');
+                i++;
+        }
+        return ((number * sign));
+}
+
+int	size_int(long long n)
+{
+	int size;
+	int div;
+
+	size = 1;
+	div = 1;
+	if (n < 0)
+	{
+		size++;
+		n *= -1;
+	}
+	while (n / div > 9)
+	{
+		div *= 10;
+		size++;
+	}
+	return (size);
+}
+
+
+/*
+int main ()
+{
+	count_width("5");
+	write(1, "n", 1);
+}
+*/
