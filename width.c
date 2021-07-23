cbc8ef22c8c6b6f9032d87adbf54292f65d17c68
Dimitri YOULA <dimitriyoula@gmail.com>
2021-07-23 19:19:55 +0200

    back  width.c

--- width.c
+++ width.c
@@ -1,31 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   is_part.c                                          :+:      :+:    :+:   */
+/*   width.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dyoula <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/28 17:42:30 by dyoula            #+#    #+#             */
-/*   Updated: 2021/07/23 19:14:57 by dyoula           ###   ########.fr       */
+/*   Created: 2021/07/23 19:18:21 by dyoula            #+#    #+#             */
+/*   Updated: 2021/07/23 19:19:31 by dyoula           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	is_spec(char c, container *box)
+// compte la longueur du nbr
+int	count_width(char *str)
 {
-	const char	spec [] = "cspdiuxX";
-	char		*list;
+	int	n;
+	int	i;
 
-	list = spec;
-	while (*list)
+	n = 0;
+	i = -1;
+	if (*str == '0')
+		str++;
+	n = ft_atoi (str);
+	while (++i < n)
 	{
-		if (*list == c)
-		{
-			box->spec == *list; // on precise le spec. 
-			return (1);
-		}
-		list++;
+		if ()
+			ft_putchar(' ');
 	}
-	return (0);
+	return (n);
 }
