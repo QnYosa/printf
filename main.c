d90c00f68467065308bc9bfb03ebf1e400736306
Dimitri YOULA <dimitriyoula@gmail.com>
2021-08-05 22:01:02 +0200

    0 en cours

--- main.c
+++ main.c
@@ -8,8 +8,8 @@ int main ()
 	int *u;
 	int n = 45;
 	u = &n;
-	printf("hello %d\n", ft_printf(" %#x ", -1));
-	printf("hello %d\n", printf(" %#x ", -1));
+	printf("hello %d\n", ft_printf(" %04d ", 9));
+	printf("hello %d\n", printf(" %04d ", 9));
 	//printf("hello %d\n", printf("%d\n", 10));
 	//printf("hello %d\n", printf("%10d\n",-9));
 	//printf("hello %d\n", printf("salut %o\n", -42));
--- struct_width_display.c
+++ struct_width_display.c
@@ -6,7 +6,7 @@
 /*   By: dyoula <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/25 15:16:12 by dyoula            #+#    #+#             */
-/*   Updated: 2021/08/02 19:14:17 by dyoula           ###   ########.fr       */
+/*   Updated: 2021/08/05 21:51:56 by dyoula           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,14 @@ void	assign_len(t_container *box)
 void	print_width(t_container *box)
 {
 	unsigned int	i;
+	int				result;
 
 	assign_len(box);
 	i = 0;
+	result = box->width - box->len;
 	if (box->zero == 0 && box->spec != 'p')
 	{
-		while (i < (box->width - box->len))
+		while ((int)i < (result))
 		{
 			box->printed += ft_putchar(' ');
 			i++;
@@ -40,7 +42,7 @@ void	print_width(t_container *box)
 	}
 	else if (box->zero == 1 && box->letters == 0)
 	{
-		while (i < box->width - box->len)
+		while ((int)i < (result))
 		{
 			box->printed += ft_putchar('0');
 			i++;
@@ -50,16 +52,19 @@ void	print_width(t_container *box)
 
 void	struct_disp_maestro(t_container *box)
 {
-	if (box->minus == 0 && (box->number > 0 || box->uli > 0) && box->zero == 0
+	if (box->minus == 0 && (box->number >= 0 || box->uli >= 0 || box->ui >= 0)
 		&& box->width_found == 1)
 		print_width(box);
-	if (box->minus == 0 && (box->number < 0 || box->uli < 0) && box->zero == 0
-		&& box->width_found == 1)
+	else if (box->minus == 0 && (box->number < 0 || box->uli < 0)
+		&& box->zero == 0 && box->width_found == 1)
 	{
 		if (box->width > 0)
 			box->width -= 1;
 		print_width(box);
 	}
+	else if (box->minus == 0 && box->zero == 1 &&
+		(box->number >= 0 || box->ui >= 0|| box->uli >= 0) && box->width_found == 1)
+		print_width(box);
 /*
 	if (box->letters == '1')
 		print_precision_l(box);
